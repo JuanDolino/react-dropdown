c43d84f603d53d8c2ea0c3382cfa88ac834aced4
Juan Vitor <juandolino8@gmail.com>
2020-05-30 16:53:17 -0300

    Finished dropdown function.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,6 +5,17 @@ import { FaChevronDown } from 'react-icons/fa';
 import './styles.css'
 
 class Header extends Component {
+
+  showDropdown = () => {
+    const dropdown = document.querySelector(".dropdown");
+    dropdown.classList.add("show-dropdown");
+  }
+
+  removeDropdown = () => {
+    const dropdown = document.querySelector(".dropdown");
+    dropdown.classList.remove("show-dropdown");
+  }
+
   render() {
     return (
       <header>
@@ -12,7 +23,23 @@ class Header extends Component {
         
         <ul>
           <li>Home</li>
-          <li>Downloads <FaChevronDown style={{color: "#000", marginLeft: "3px"}}/></li>
+          <li 
+            onMouseOver={this.showDropdown}
+            onMouseOut={this.removeDropdown}>
+            Downloads
+            <FaChevronDown
+              style={{ color: "#7d8597", marginLeft: "3px" }}
+              size={12} />
+
+            <div className="dropdown">
+              <div className="row">
+                <p>MacOS and Windows</p>
+              </div>
+              <div className="row">
+                <p>Ios and Android</p>
+              </div>
+            </div>
+          </li>
           <li>About</li>
         </ul>
       </header>
